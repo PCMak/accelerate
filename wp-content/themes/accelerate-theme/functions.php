b8ceca8090c7e65cc92fd8afc8b003e73e05cff1
PC Mak <pamelamakster@gmail.com>
2015-11-17 23:22:17 -0800

    child theme changes 3x

--- wp-content/themes/accelerate-theme/functions.php
+++ wp-content/themes/accelerate-theme/functions.php
@@ -27,3 +27,18 @@
 
 register_nav_menu('top-nav', 'Top Nav');
 register_nav_menu('social-media', 'Social Media Nav');
+
+// <! -- Loading all style.css via Parent Theme -->
+// 
+// add_action( 'wp_enqueue_scripts', 'my_enqueue_styles' );
+// 
+// function my_enqueue_styles() {
+// 
+//     /* If using a child theme, auto-load the parent theme style. */
+//     if ( is_child_theme() ) {
+//         wp_enqueue_style( 'parent-style', trailingslashit( get_template_directory_uri() ) . 'style.css' );
+//     }
+// 
+//     /* Always load active theme's style.css. */
+//     wp_enqueue_style( 'style', get_stylesheet_uri() );
+// }
